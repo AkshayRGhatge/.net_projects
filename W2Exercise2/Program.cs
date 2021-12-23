063fd85800bc4f03aa4e9e0caed759e9237b82f7
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:06:18 -0500

    Adding Week 2 Exercise2

---
+++ W2Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace W2Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Monthly Investment");
+            double monthlyInvest = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter yearly Interest Rate");
+            double yearlyInterest = double.Parse(Console.ReadLine());
+
+            Console.Write("Number of years");
+            int numberOfYears = int.Parse(Console.ReadLine());
+
+            double monthlyInterest = yearlyInterest / 12 / 100;
+            int numberOfmonths = numberOfYears * 12;
+            double futureValue = 0.0;
+
+            for (int i = 0; i < numberOfmonths; i++)
+            {
+                futureValue = (futureValue + monthlyInvest) * (1 + monthlyInterest);
+            }
+            Console.WriteLine("\n Future Value:"+futureValue.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
