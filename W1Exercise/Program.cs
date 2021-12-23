0232e1b4575a1a9f32d83a054ed112b4f900903a
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:04:02 -0500

    Adding Exercise 1

---
+++ W1Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace W1Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
