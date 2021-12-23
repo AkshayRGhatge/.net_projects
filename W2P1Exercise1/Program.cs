2268ceeaff0d2e1adfa27d79d003ef6f042938d3
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:08:40 -0500

    Week2 p1 Exercise1

---
+++ W2P1Exercise1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace W2P1Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter type customer C or R");
+            char custType = Console.ReadLine().ToUpper()[0];
+
+            Console.WriteLine("Enter subtotal");
+            double subTotal = double.Parse(Console.ReadLine());
+
+            double discountPercent = 0.0;
+
+            switch (custType)
+            {
+                case 'R':
+                    if (subTotal >= 250)
+                        discountPercent = 0.25;
+                    else if (subTotal >= 100 && subTotal < 250)
+                        discountPercent = 0.1;
+                    break;
+
+                case 'C':
+                    if (subTotal >= 250)
+                        discountPercent = 0.3;
+                    else
+                        discountPercent = 0.2;
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid customer type");
+                    break;
+            }
+
+                    double discountAmount = subTotal * discountPercent;
+                    double total = subTotal - discountAmount;
+
+
+                    Console.WriteLine("Discount Percent" + discountPercent.ToString("P1"));
+                    Console.WriteLine("Discount Amount" + discountAmount.ToString("C"));
+                    Console.WriteLine("Total" + total.ToString("C"));
+                    Console.ReadKey();
+
+
+            }
+        }
+    }
+
