cc0b2f75af534f17a2dc5c986e71793a6c9d1e9d
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:07:24 -0500

    Adding Week2 Exercise4

---
+++ W2Exercise4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace W2Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N\t10N\t100N\t1000N");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int ten = 10 * i;
+                int hundred = 100 * i;
+                int thousand = 1000 * i;
+                Console.WriteLine(i+"\t"+ten+"\t"+hundred+"\t"+thousand);
+            }
+            Console.ReadKey();
+        }
+    }
+}
