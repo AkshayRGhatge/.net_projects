e68a10015aee94a65191600e6328f732092fb074
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:03:17 -0500

    Adding W1 Exercise2

---
+++ W1Execise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace W1Execise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number\t|square\t|cube");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i+"\t"+(i*i)+"\t"+(i*i*i));
+                Console.ReadLine();
+            }
+        }
+    }
+}
