15fa8607281ff182bf229fb80ac56b06cb599efc
AkshayRGhatge <96392298+AkshayRGhatge@users.noreply.github.com>
2021-12-23 16:05:34 -0500

    Week1 Exercise4

---
+++ W1Exercise4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace W1Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndnum = rnd.Next(10);
+            int userguess;
+            do
+            {
+                Console.WriteLine("Guess the number between 1-10");
+                 userguess = int.Parse(Console.ReadLine());
+                if (userguess == rndnum)
+                {
+                    Console.WriteLine("Correct Answer");
+                }
+                else if (userguess < rndnum)
+                {
+                    Console.WriteLine("Too low");
+
+                }
+                else
+                {
+                    Console.WriteLine("Too high");
+                }
+            } while (userguess != rndnum);
+        
+        }
+        }
+        }
+
